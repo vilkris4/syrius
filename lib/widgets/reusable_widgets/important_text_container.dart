2fb420d28193e246465976b92ab7d654e092263a
vilkris <vilkris@protonmail.com>
2023-04-26 16:02:18 +0300

    Add ImportantTextContainer widget

---
+++ lib/widgets/reusable_widgets/important_text_container.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:zenon_syrius_wallet_flutter/utils/app_colors.dart';
+
+class ImportantTextContainer extends StatelessWidget {
+  final String text;
+  final bool showBorder;
+
+  const ImportantTextContainer({
+    required this.text,
+    this.showBorder = false,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: const Color(0xff282828),
+        border: showBorder
+            ? Border.all(
+                width: 1.0,
+                color: AppColors.errorColor,
+              )
+            : null,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(8.0),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(15.0, 20.0, 15.0, 20.0),
+        child: Row(
+          children: [
+            const Icon(
+              Icons.info,
+              size: 20.0,
+              color: Colors.white,
+            ),
+            const SizedBox(
+              width: 15.0,
+            ),
+            Text(text, style: const TextStyle(fontSize: 14.0, height: 1)),
+          ],
+        ),
+      ),
+    );
+  }
+}
