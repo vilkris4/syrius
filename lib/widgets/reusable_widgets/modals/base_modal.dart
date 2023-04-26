0c1530cfc39a63030f691626bbbcb36be0886344
vilkris <vilkris@protonmail.com>
2023-04-26 16:04:00 +0300

    Make BaseModal scrollable

--- lib/widgets/reusable_widgets/modals/base_modal.dart
+++ lib/widgets/reusable_widgets/modals/base_modal.dart
@@ -15,56 +15,63 @@ class BaseModal extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: AnimatedSize(
-        duration: const Duration(milliseconds: 100),
-        curve: Curves.easeInOut,
-        child: Container(
-          width: 570.0,
-          color: Theme.of(context).colorScheme.primaryContainer,
-          child: Padding(
-            padding: const EdgeInsets.all(25.0),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+        maxHeight: MediaQuery.of(context).size.height * 0.8,
+      ),
+      child: SingleChildScrollView(
+        child: Material(
+          child: AnimatedSize(
+            duration: const Duration(milliseconds: 100),
+            curve: Curves.easeInOut,
+            child: Container(
+              width: 570.0,
+              color: Theme.of(context).colorScheme.primaryContainer,
+              child: Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
                   children: [
-                    Column(
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Text(
-                          title,
-                          style: const TextStyle(
-                            fontSize: 18.0,
-                          ),
+                        Column(
+                          children: [
+                            Text(
+                              title,
+                              style: const TextStyle(
+                                fontSize: 18.0,
+                              ),
+                            ),
+                            Visibility(
+                              visible: subtitle.isNotEmpty,
+                              child: const SizedBox(
+                                height: 3.0,
+                              ),
+                            ),
+                            Visibility(
+                              visible: subtitle.isNotEmpty,
+                              child: Text(subtitle),
+                            ),
+                          ],
                         ),
-                        Visibility(
-                          visible: subtitle.isNotEmpty,
-                          child: const SizedBox(
-                            height: 3.0,
+                        MouseRegion(
+                          cursor: SystemMouseCursors.click,
+                          child: GestureDetector(
+                            onTap: () => Navigator.of(context).pop(),
+                            child: const Icon(
+                              Icons.clear,
+                              color: AppColors.lightSecondary,
+                              size: 22.0,
+                            ),
                           ),
-                        ),
-                        Visibility(
-                          visible: subtitle.isNotEmpty,
-                          child: Text(subtitle),
                         ),
                       ],
                     ),
-                    MouseRegion(
-                      cursor: SystemMouseCursors.click,
-                      child: GestureDetector(
-                        onTap: () => Navigator.of(context).pop(),
-                        child: const Icon(
-                          Icons.clear,
-                          color: AppColors.lightSecondary,
-                          size: 22.0,
-                        ),
-                      ),
-                    ),
+                    child
                   ],
                 ),
-                child
-              ],
+              ),
             ),
           ),
         ),
