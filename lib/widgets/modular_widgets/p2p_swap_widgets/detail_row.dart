cbed9144fbcdb3f314497aada83d4e8e9605373a
vilkris <vilkris@protonmail.com>
2023-04-26 16:08:22 +0300

    Add DetailRow widget

---
+++ lib/widgets/modular_widgets/p2p_swap_widgets/detail_row.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:zenon_syrius_wallet_flutter/utils/app_colors.dart';
+import 'package:zenon_syrius_wallet_flutter/widgets/reusable_widgets/icons/copy_to_clipboard_icon.dart';
+
+class DetailRow extends StatelessWidget {
+  final String label;
+  final String value;
+  final String? valueToShow;
+  final bool canBeCopied;
+
+  const DetailRow({
+    required this.label,
+    required this.value,
+    this.valueToShow,
+    this.canBeCopied = true,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(label,
+            style: const TextStyle(
+                fontSize: 12.0, color: AppColors.subtitleColor)),
+        Row(
+          children: [
+            Text(valueToShow ?? value,
+                style: const TextStyle(
+                    fontSize: 12.0, color: AppColors.subtitleColor)),
+            Visibility(
+              visible: canBeCopied,
+              child: CopyToClipboardIcon(
+                value,
+                iconColor: AppColors.subtitleColor,
+                materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                padding: const EdgeInsets.only(left: 8.0),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+}
