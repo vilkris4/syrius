01d6a39b4c4a96f7c8da1609695551e411cbd813
vilkris <vilkris@protonmail.com>
2023-05-01 10:56:09 +0300

    Disable P2pSwapOptionButtons when wallet is generating plasma

--- lib/widgets/modular_widgets/p2p_swap_widgets/p2p_swap_options_card.dart
+++ lib/widgets/modular_widgets/p2p_swap_widgets/p2p_swap_options_card.dart
@@ -1,13 +1,18 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:zenon_syrius_wallet_flutter/blocs/pow_generating_status_bloc.dart';
+import 'package:zenon_syrius_wallet_flutter/main.dart';
 import 'package:zenon_syrius_wallet_flutter/utils/app_colors.dart';
+import 'package:zenon_syrius_wallet_flutter/utils/toast_utils.dart';
 import 'package:zenon_syrius_wallet_flutter/widgets/modular_widgets/p2p_swap_widgets/modals/join_native_swap_modal.dart';
 import 'package:zenon_syrius_wallet_flutter/widgets/modular_widgets/p2p_swap_widgets/modals/native_p2p_swap_modal.dart';
 import 'package:zenon_syrius_wallet_flutter/widgets/modular_widgets/p2p_swap_widgets/modals/start_native_swap_modal.dart';
-import 'package:zenon_syrius_wallet_flutter/widgets/reusable_widgets/buttons/swap_options_button.dart';
+import 'package:zenon_syrius_wallet_flutter/widgets/modular_widgets/p2p_swap_widgets/p2p_swap_options_button.dart';
 import 'package:zenon_syrius_wallet_flutter/widgets/reusable_widgets/dialogs/dialogs.dart';
+import 'package:zenon_syrius_wallet_flutter/widgets/reusable_widgets/error_widget.dart';
 import 'package:zenon_syrius_wallet_flutter/widgets/reusable_widgets/layout_scaffold/card_scaffold.dart';
+import 'package:znn_sdk_dart/znn_sdk_dart.dart';
 
 class P2pSwapOptionsCard extends StatefulWidget {
   const P2pSwapOptionsCard({
@@ -37,9 +42,18 @@ class _P2pSwapOptionsCardState extends State<P2pSwapOptionsCard> {
   }
 
   Widget _getWidgetBody(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(20.0),
-      child: _getNativeOptions(),
+    return StreamBuilder<PowStatus>(
+      stream: sl.get<PowGeneratingStatusBloc>().stream,
+      builder: (_, snapshot) {
+        if (snapshot.hasError) {
+          return SyriusErrorWidget(snapshot.error!);
+        }
+        final isGeneratingPlasma = _isGeneratingPlasma(snapshot.data);
+        return Container(
+          margin: const EdgeInsets.all(20.0),
+          child: _getNativeOptions(isGeneratingPlasma),
+        );
+      },
     );
   }
 
@@ -55,27 +69,33 @@ class _P2pSwapOptionsCardState extends State<P2pSwapOptionsCard> {
     );
   }
 
-  Column _getNativeOptions() {
+  Column _getNativeOptions(bool isGeneratingPlasma) {
     return Column(
       children: [
-        SwapOptionsButton(
+        P2pSwapOptionsButton(
           primaryText: 'Start swap',
           secondaryText: 'Start a native swap with a counterparty.',
-          onClick: () => showCustomDialog(
-            context: context,
-            content: StartNativeSwapModal(onSwapStarted: _showNativeSwapModal),
-          ),
+          onClick: () => isGeneratingPlasma
+              ? _showGeneratingPlasmaToast()
+              : showCustomDialog(
+                  context: context,
+                  content:
+                      StartNativeSwapModal(onSwapStarted: _showNativeSwapModal),
+                ),
         ),
         const SizedBox(
           height: 25.0,
         ),
-        SwapOptionsButton(
+        P2pSwapOptionsButton(
           primaryText: 'Join swap',
           secondaryText: 'Join a native swap started by a counterparty.',
-          onClick: () => showCustomDialog(
-            context: context,
-            content: JoinNativeSwapModal(onJoinedSwap: _showNativeSwapModal),
-          ),
+          onClick: () => isGeneratingPlasma
+              ? _showGeneratingPlasmaToast()
+              : showCustomDialog(
+                  context: context,
+                  content:
+                      JoinNativeSwapModal(onJoinedSwap: _showNativeSwapModal),
+                ),
         ),
         const SizedBox(
           height: 40.0,
@@ -109,9 +129,19 @@ class _P2pSwapOptionsCardState extends State<P2pSwapOptionsCard> {
                 ),
               ],
             ),
+            // TODO: Open link to tutorial
+            onTap: () => ToastUtils.showToast(context, 'No tutorial yet'),
           ),
         ),
       ],
     );
+  }
+
+  bool _isGeneratingPlasma(PowStatus? status) {
+    return status != null && status == PowStatus.generating;
+  }
+
+  void _showGeneratingPlasmaToast() {
+    ToastUtils.showToast(context, 'Please wait while Plasma is generated');
   }
 }
